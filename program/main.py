44a85dc6c065f396e9b21d95094c8a73ab4acd38
Tonko <tbosse20@student.aau.dk>
2021-09-21 12:28:31 +0200

    Server and client start. Missing send package to know what is being sent

--- program/main.py
+++ program/main.py
@@ -1,3 +1,25 @@
-import cv2
+# https://www.tutorialspoint.com/python/python_networking.htm
+import socket, pickle               # Import socket module
 
-print("Hello World")
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 1024                 # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+print(s)
+print('Running')
+
+players = {}
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print('Got connection from:', addr)
+   try:
+      c.send(b'Thank you for connecting')
+   except TypeError:
+      print('TypeError')
+
+      recive = c.recv(1024)
+      message = pickle.loads(recive)
+      print('Client sent:', repr(message))
+c.close()
