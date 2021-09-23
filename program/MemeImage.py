ad90d8fe99f4183b43533b70be77b723b224c76f
Charlotte1997AAU <80900771+Charlotte1997AAU@users.noreply.github.com>
2021-09-23 12:54:00 +0200

    Update MemeImage.py

--- program/MemeImage.py
+++ program/MemeImage.py
@@ -1,2 +1,17 @@
+import random
+import os
+import cv2
+
 class MemeImage:
-    pass
+
+    def getRandomImage():
+        path = "C:\\Users\\Charlotte Johansen\\PycharmProjects\\miniprojectProgramming\\.memes"
+        files = os.listdir(path)
+        d = random.choice(files)
+         # os.startfile(d)
+        ran = cv2.imread(d)
+        ran = cv2.resize(ran, (640, 640))
+        cv2.imshow("hej", ran)
+        cv2.waitKey(0)
+
+    getRandomImage()
