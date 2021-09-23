2214dc7719fb9d42305f240ed67929ef2d4cae30
Tonko <tbosse20@student.aau.dk>
2021-09-23 13:14:14 +0200

    MemeImage class get a random image adjustments

--- program/MemeImage.py
+++ program/MemeImage.py
@@ -2,22 +2,20 @@ import random
 import os
 import cv2
 
-class RandomMemeImage:
-
-    def getRandomImage():
-        path = "C:\\Users\\Charlotte Johansen\\PycharmProjects\\miniprojectProgramming\\.memes"
-        files = os.listdir(path)
-        d = random.choice(files)
-         # os.startfile(d)
-        ran = cv2.imread(d)
-        ran = cv2.resize(ran, (640, 640))
-        cv2.imshow("hej", ran)
-        cv2.waitKey(0)
-
-    getRandomImage()
-
 class MemeImage:
-    images = ['star.png']
+    path = "C:\\Users\\Charlotte Johansen\\PycharmProjects\\miniprojectProgramming\\.memes"
 
     def __init__(self):
         self.image = random.choice(self.images)
+
+    def getRandomImage(self, path: str) -> str:
+        files = os.listdir(path)
+        d = random.choice(files)
+         # os.startfile(d)
+        #ran = cv2.imread(d)
+        ran = d
+        #ran = cv2.resize(ran, (640, 640))
+        #cv2.imshow("hej", ran)
+        #cv2.waitKey(0)
+
+        return ran
