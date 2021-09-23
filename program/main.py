4acce4eb110f035860ba4f4bc04467680ca46e06
Sebastian Whitehead <sebastianmikael2000@gmail.com>
2021-09-23 14:54:46 +0200

    Image Editing
    Testing Image

--- program/main.py
+++ program/main.py
@@ -1,7 +1,22 @@
-from Server import Server
+# Importing the PIL library
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
-def __main__():
-    server = Server(1024)
+# Open an Image
+img = Image.open('car.png')
 
-if __name__ == '__main__':
-    __main__()
+# Call draw Method to add 2D graphics in an image
+I1 = ImageDraw.Draw(img)
+
+# Custom font style and font size
+myFont = ImageFont.truetype('ComicSansMS3.ttf', 65)
+
+# Add Text to an image
+I1.text((10, 10), "The Quick Brown Fox Jumped Over The Lazy Dog", font=myFont, fill=(255, 150, 60))
+
+# Display edited image
+img.show()
+
+# Save the edited image
+img.save("car2.png")
