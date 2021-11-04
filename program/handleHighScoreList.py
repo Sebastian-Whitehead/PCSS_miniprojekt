de56988c4a97892193515660fe7911c9ea53d25b
Tonko <tbosse20@student.aau.dk>
2021-11-04 13:36:31 +0100

    Comments and clean up

--- program/handleHighScoreList.py
+++ program/handleHighScoreList.py
@@ -1,6 +1,8 @@
 import json, random
 
 
+# Load the data from the txt-file using JSON.
+# Return an empty list if the file is empty
 def loadScores(fileName):
     try:
         with open(fileName) as json_file:
@@ -9,28 +11,34 @@ def loadScores(fileName):
         return []
 
 
+# Insert the data into the old high score list,
+# using split inserting.
+# (Slice the list in two until the desired location has been found
 def insertScore(data, oldScoreList):
     # Iterate all new scores
     for newScore in data:
+        newValue = newScore[1]  # Get the value from the new score
+
         compareList = list(oldScoreList)  # Copy the score list to comparing list
 
-        if len(compareList) < 1: oldScoreList.insert(0, newScore)
+        # Insert the element if the old list is empty
+        if len(compareList) < 1:
+            oldScoreList.insert(0, newScore)  # Insert current element
+            continue  # Continue to next iteration
 
         # Cut the compare list in half
         while len(compareList) > 0:
-            newValue = newScore[1]  # Get the value from the new score
 
-            # Get compareing data
+            # Get comparing data
             compareIndex = int(len(compareList) / 2)  # Get the index of comparing score
-            compareValue = compareList[compareIndex][1]  # Get the value of comparing score
+            actualElement = compareList[compareIndex]  # Get the actual element of comparing
+            compareValue = actualElement[1]  # Get the value of comparing
+            actualIndex = oldScoreList.index(actualElement)  # Get the actual index of the element
 
             # Check if the values are the same
             if compareValue == newValue:
-                compareScore = [x for x in oldScoreList if x[1] == compareValue][0]
-                finalIndex = oldScoreList.index(compareScore)
-                # Insert the score at the current index
-                oldScoreList.insert(finalIndex, newScore)
-                break
+                oldScoreList.insert(actualIndex, newScore)  # Insert the score at the current index
+                break  # Break the while loop
 
             # Compare the value with the new value
             if compareValue > newValue:
@@ -40,29 +48,27 @@ def insertScore(data, oldScoreList):
 
             # Insert the new value into the score list at the correct index
             if len(compareList) < 1:
-                compareScore = [x for x in oldScoreList if x[1] == compareValue][0]
-                finalIndex = oldScoreList.index(compareScore)
-                if compareValue < newValue: finalIndex += 1
-                oldScoreList.insert(finalIndex, newScore)
-                continue
+                if compareValue < newValue: actualIndex += 1  # Add one to place it behind the compare element
+                oldScoreList.insert(actualIndex, newScore)  # Insert the current score in the score list
 
-    return oldScoreList
+    return oldScoreList  # return the updated list
 
 
+# Update the txt file with the new data
+# - Load file with old high score list
+# - Insert the given data into the list
+# - Overwrite the next high score list in the same txt-file
 def saveScores(fileName, scoreList):
     oldScoreList = loadScores(fileName)  # Load file
-
-    # Insert data into score list
-    scoreList = insertScore(scoreList, oldScoreList)
-
-    # Update the score list
-    updatedScoreList = scoreList
+    updatedScoreList = insertScore(scoreList, oldScoreList)  # Insert data into score list
 
     # Save the updated list as json
     with open(fileName, 'w') as outfile:
         json.dump(updatedScoreList, outfile)
 
 
+# Test the program with manufactured data.
+# Save and load data
 def programTesting(data, printOut):
     fileName = 'allTimeHighScore.txt'
 
